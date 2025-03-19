b618fbe544d2eccafd11621e92883795df76fba6
Daniil <111095329+rythmmm@users.noreply.github.com>
2025-03-19 22:07:20 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
+const hamburgerMenuText = document.getElementById("hamburgerMenuText")
+const hamburgerMenu = document.getElementById("hamburgerMenu")
+const menuNavItemContainer = document.getElementById("menuNavItemContainer")
+const menuNavItems = document.querySelectorAll(".menuNavItem")
+
+const menuNavItemsTexts = ["Models", "Innovation", "Careers", "Sustainability", "Audi in sport"]
+
+document.getElementById("hamburgerBtn").addEventListener("click", () => {
+
+if (hamburgerMenu.style.display === "none" || hamburgerMenu.style.display === "") {
+	hamburgerMenu.style.display = "flex"
+	hamburgerMenuText.style.display = "flex"
+	hamburgerMenuText.textContent = "Menu"
+	menuNavItems.forEach((item, index) => {
+		item.textContent = menuNavItemsTexts[index]
+	})
+} else {
+	hamburgerMenu.style.display = "none"
+	hamburgerMenuText.style.display = "none"
+}
+})
+
 const backToTopBtn = document.getElementById("backToTopBtn")
 
 backToTopBtn.onclick = function() {
